c24d74e02ec461e55bc69b3b0de184ad26356340
gamikasa <73681995+gamikasa@users.noreply.github.com>
2020-10-30 12:53:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-echo "<h2>Hello world</h2>";
+echo "<h2>Rammakorn Wongjun</h2>";
 <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fdogelore.fandom.com%2Fwiki%2FCheemsburbger&psig=AOvVaw1p9FvMbUep_NpYY_i6dXEd&ust=1604123431503000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNjC5dHP2-wCFQAAAAAdAAAAABAD">
 echo "Hello world654321<br>";
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.redbubble.com%2Fi%2Fphotographic-print%2Fdoge-cheems-hug-by-moonman1232141%2F45380914.6Q0TX&psig=AOvVaw1p9FvMbUep_NpYY_i6dXEd&ust=1604123431503000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNjC5dHP2-wCFQAAAAAdAAAAABAY">
 ?>
