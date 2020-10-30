d4f372950c6c7bf7fe504093e3c76a4c6669493b
gamikasa <73681995+gamikasa@users.noreply.github.com>
2020-10-30 12:58:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
 echo "<h2>Rammakorn Wongjun</h2>";
-<img src="https://static.wikia.nocookie.net/dogelore/images/8/87/411.png/revision/latest/top-crop/width/360/height/450?cb=20200330152532">
 echo "Hello world654321<br>";
-<img src="https://ih1.redbubble.net/image.1087240082.0914/flat,750x,075,f-pad,750x1000,f8f8f8.jpg">
 ?>
