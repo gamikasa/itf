bf2a271105d7b9d5f8b0006839f17d0b0180f8a7
gamikasa <73681995+gamikasa@users.noreply.github.com>
2020-10-30 18:47:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,9 @@
     <body>
         <h1>My First Heading</h1>
         <p>My First Paragraph</p>
-        <img scr="sheems.jpg">
+        <img src="sheems.jpg">
         <br>
         <a href="http://www.kmtil.ac.th">This is a link</a>
-
         <h2>The unordered list</h2>
         <u1>
           <li>Google</li>
