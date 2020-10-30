32226fc6d74056ae207fc6a5f0d644b154700ce6
gamikasa <73681995+gamikasa@users.noreply.github.com>
2020-10-30 12:56:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h2>Rammakorn Wongjun</h2>";
-<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fdogelore.fandom.com%2Fwiki%2FCheemsburbger&psig=AOvVaw1p9FvMbUep_NpYY_i6dXEd&ust=1604123431503000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNjC5dHP2-wCFQAAAAAdAAAAABAD">
+<img src="https://static.wikia.nocookie.net/dogelore/images/8/87/411.png/revision/latest/top-crop/width/360/height/450?cb=20200330152532">
 echo "Hello world654321<br>";
-<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.redbubble.com%2Fi%2Fphotographic-print%2Fdoge-cheems-hug-by-moonman1232141%2F45380914.6Q0TX&psig=AOvVaw1p9FvMbUep_NpYY_i6dXEd&ust=1604123431503000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNjC5dHP2-wCFQAAAAAdAAAAABAY">
+<img src="https://ih1.redbubble.net/image.1087240082.0914/flat,750x,075,f-pad,750x1000,f8f8f8.jpg">
 ?>
