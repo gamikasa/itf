e35a646e79646eec7cf41fe67d218b9cb7e9ca60
gamikasa <73681995+gamikasa@users.noreply.github.com>
2020-10-30 12:50:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 echo "<h2>Hello world</h2>";
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fdogelore.fandom.com%2Fwiki%2FCheemsburbger&psig=AOvVaw1p9FvMbUep_NpYY_i6dXEd&ust=1604123431503000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNjC5dHP2-wCFQAAAAAdAAAAABAD">
 echo "Hello world654321<br>";
 ?>
